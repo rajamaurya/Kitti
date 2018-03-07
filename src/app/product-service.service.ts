6659fae0b8d16ba522c5e759861f7e7116ee095a
Raja Krishna Maurya <krishna.raja78@gmail.com>
2018-03-07 15:19:28 +0530

    Update product-service.service.ts

--- src/app/product-service.service.ts
+++ src/app/product-service.service.ts
@@ -2,28 +2,27 @@ import { Injectable } from '@angular/core';
 import {Kittie} from './kittie';
 import {KittieListComponent} from './kittie-list/kittie-list.component';
 import {KITTIES} from './mock-kitties';
+import { Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/Observable/throw';
+import '../assets/json.json';
+
 
 @Injectable()
 export class ProductServiceService {
 
-  kittie: Kittie;
-  kitties = KITTIES;
-  SelectedKittie: Kittie;
-  isSelected = false;
-  isPaymentSelected = false;
-  products = [];
-  productCount = 0;
-  totalPrice = 0;
-  constructor() { }
-  getProductDetails(kittie: Kittie): any {
-
-    this.SelectedKittie = kittie;
-    this.isSelected = true;
-    // alert(this.isSelected);
-    this.products.push(kittie);
-    if (kittie.id > 1) {
-      this.productCount++;
-      this.totalPrice += kittie.price;
-    }
+  constructor(private _http: Http) {  }
+  
+  /*getProduts(): Observable<Kittie[]>{
+    return this._http.get("http://localhost:4200/kittie").map((response: Response) => <Kittie[]>response.json()).catch(this.handleError);
+  } */
+  getProductById(id: number): Observable<Kittie>{
+   // alert(12232);
+     return this._http.get('../assets/json.json')
+      .map((response: Response) => (<Kittie>response.json()) , (error) => {alert("Hi error" + error);}); 
+  /*  return this._http.get('http://localhost:4200/kittie/' + id )
+      .map((response: Response) => <Kittie>response.json()); */
   }
 }
